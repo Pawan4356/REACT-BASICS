be9c503f810e845f3ee7414e0cbd59010fc92cef
Pawan4356 <pawankumar27112005@gmail.com>
2025-12-23 13:51:45 +0530

    Tailwind and props in React

--- 4_TailwindAndProps/src/components/card.jsx
+++ 4_TailwindAndProps/src/components/card.jsx
@@ -7,7 +7,7 @@ function Card({ values }) {
     } = values || {};
 
     return (
-        <div className="relative h-[400px] w-[300px] rounded-[20px] m-[10px]">
+        <div className="relative h-[400px] w-[300px] rounded-[20px] m-2.5">
             <img
                 src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
                 alt="AirMax Pro"
